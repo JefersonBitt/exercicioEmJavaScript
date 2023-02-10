f02a15179d16073cba0410ae23da4537828ad0be
Jeferson Bittencourt <jefersonbittencourt15@gmail.com>
2023-02-09 23:21:07 -0300

    exercicioEmJavaScript

---
+++ exercicioCadastroRecruta/index.js
@@ -0,0 +1,20 @@
+
+
+let nome = prompt("Insira o seu nome:")
+let sobrenome = prompt("Insira o seu sobrenome:")
+let campoEstudo = prompt("Insira o campo de estudo:")
+let anoNascimento = prompt("Insira o Ano de Nascimento")
+let idade = 2023 - anoNascimento
+
+console.log(nome)
+console.log(sobrenome)
+console.log(campoEstudo)
+console.log(anoNascimento)
+console.log(idade)
+
+alert("Recruta cadastrado com Sucesso!\n" +
+    "\nNome completo:" + nome + " " + sobrenome +
+    "\nCampo de Estudos:" + campoEstudo +
+    "\nIdade:" + idade)
+
+
---
+++ exercicioCalculadoraQuatroOp/index.js
@@ -0,0 +1,26 @@
+
+
+let num1 = parseFloat(prompt("Insira um número"))
+
+let num2 = parseFloat(prompt("Insira outro número"))
+
+
+console.log(num1)
+console.log(num2)
+
+let soma = num1 + num2
+let sub = num1 - num2
+let mult = num1 * num2
+let div = num1 / num2
+
+console.log(soma)
+console.log(sub)
+console.log(mult)
+console.log(div)
+
+alert("O Resultado das 4 Operacoes é:\n" +
+    "\nSoma:" + soma +
+    "\nSubtracao:" + sub +
+    "\nMultiplicacao:" + mult +
+    "\nDivisao:" + div)
+
